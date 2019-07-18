73500fc243d2fddb7d2ba7adeb2184b6b0d64272
njenya <njenya@gmail.com>
2019-07-18 16:13:43 +0300

    compleate task3

--- src/ui/Task3/Task3.jsx
+++ src/ui/Task3/Task3.jsx
@@ -1,11 +1,61 @@
 import React from 'react'
+import MySingleton from './singletonObject'
+import styles from "../Task3/Task3.module.css";
+import codeImg from '../../assets/code.jpg'
 
-function Task3() {
-	return (
-		<div>
-			Task3
-		</div>
-	);
+class Task3 extends React.Component {
+
+	state = {
+		a: {},
+		b: {},
+		c: {},
+		d: {}
+	}
+
+	showResult = () => {
+		this.setState({
+			a: MySingleton(),
+			b: new MySingleton(),
+			c: new MySingleton(),
+			d: MySingleton()
+		})
+		console.log(MySingleton)
+	}
+
+// Used stack: class component, local state
+	render() {
+		return (
+			<div>
+				<div>
+					<h2>Task 3</h2>
+				</div>
+				<div className={styles.area}>
+					<div className={styles.inputArea}>
+						<div>
+							<img src={codeImg}/>
+						</div>
+						<div>
+							<button onClick={this.showResult}>Show result</button>
+						</div>
+					</div>
+					<div className={styles.outputArea}>
+						<div>
+							a === b result ==> {this.state.a === this.state.b && <span>true</span>}
+						</div>
+						<div>
+							b === c result ==> {this.state.b === this.state.c && <span>true</span>}
+						</div>
+						<div>
+							c === d result ==> {this.state.c === this.state.d && <span>true</span>}
+						</div>
+						<div>
+							a === d result ==> {this.state.a === this.state.d && <span>true</span>}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Task3;
---
+++ src/ui/Task3/singletonObject.js
@@ -0,0 +1,15 @@
+let MySingleton = (function () {
+	let instance;
+	return function Construct_singletone () {
+		if (instance) {
+			return instance;
+		}
+		if (this && this.constructor === Construct_singletone) {
+			instance = this;
+		} else {
+			return new Construct_singletone();
+		}
+	}
+}());
+
+export default MySingleton
